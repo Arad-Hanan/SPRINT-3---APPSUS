8da728cc1806c1ec91630c5ca9836ef81ccb1541
Nakhsho-mancer <nakhshon.gery@gmail.com>
2026-08-22 00:25:15 +0300

    about story

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,22 @@
 
 export function About() {
     return <section className="container about">
-        <h1>About Page</h1>
+        <h1>We are two soldiers from the future, fighting a never-ending war for humanity's fate...</h1>
+        <p>
+            Our mission is to save, kill, kidnap, or otherwise deal with Sarah Connor...
+            Sadly, records were lost, so the exact nature of the mission remains disputed.
+        </p>
+        <p>
+            Our main challenges are avoiding any and all of our ancestors,
+            so we do not accidentally negate our own existence and create universe-ending paradoxes...
+        </p>
+        <p>
+            Unless, of course, it turns out one of us is his own grand-ancestor,
+            in which case we must ensure our existence...
+        </p>
+        <p>
+            Whether or not we can even influence events in this timeline, or whether it is fixed,
+            remains to be seen...
+        </p>
     </section>
 }
